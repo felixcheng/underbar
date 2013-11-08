13ada07a1a7469cc568b6ac16ecd6a65db1f9748
felixcheng <chengfelix@gmail.com>
2013-11-07 22:09:25 -0800

    wip
    wip

--- src/underbar.js
+++ src/underbar.js
@@ -15,17 +15,34 @@ var _ = { };
 
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
-  _.first = function(array, n) {
+   _.first = function(array, n) {
+    if (arguments.length === 1){
+      return array[0];
+    } else {
+      return array.slice(0, n);
+    }
+    
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (arguments.length === 1){
+      return array[array.length - 1];
+    } else {
+      if (n > array.length) {
+        n = array.length;
+      }
+      return array.slice(array.length - n, array.length);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+    for (var e in collection) {
+      iterator(collection[e], e, collection);
+    };
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -34,10 +51,23 @@ var _ = { };
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
+    for (var i = 0; i < array.length; i++){
+      if (array[i] === target){
+        return i;
+      }
+    }
+    return -1
   };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, iterator) {
+    var answer = [];
+    for (var i = 0; i < collection.length; i++){
+      if (iterator(collection[i])){
+        answer.push(collection[i]);
+      }
+    }
+    return answer;
   };
 
   // Return all elements of an array that don't pass a truth test.
